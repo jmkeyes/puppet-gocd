051a88f5217450700f8d16354a5d5fe9bcc32762
Vlad <vlad@v-lad.org>
2016-02-16 16:59:57 -0500

    Better regex for password hash

--- manifests/server/user.pp
+++ manifests/server/user.pp
@@ -10,7 +10,7 @@ define gocd::server::user (
   $name_error = "The username '${name}' is not valid."
   validate_re($name, $name_regex, $name_error)
 
-  $password_regex = '^\{[A-Z0-9]+\}[a-zA-Z0-9+\/]+={0,2}$'
+  $password_regex = '^[A-Z0-9]+[a-zA-Z0-9+\/]+={0,2}$'
   $password_error = "The password hash '${password}' is not valid."
   validate_re($password, $password_regex, $password_error)
 
