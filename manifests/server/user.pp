9366b7ee152160643c4e86962e7d862e009d9af6
Vlad <vlad@v-lad.org>
2016-02-16 16:48:30 -0500

    Puppet regexes should not be surrounded by //

--- manifests/server/user.pp
+++ manifests/server/user.pp
@@ -6,11 +6,11 @@ define gocd::server::user (
   validate_string($name)
   validate_string($password)
 
-  $name_regex = '/^[a-z0-9_-]{3,16}$/'
+  $name_regex = '^[a-z0-9_-]{3,16}$'
   $name_error = "The username '${name}' is not valid."
   validate_re($name, $name_regex, $name_error)
 
-  $password_regex = '/^\{[A-Z0-9]+\}[a-zA-Z0-9+\/]+={0,2}$/'
+  $password_regex = '^\{[A-Z0-9]+\}[a-zA-Z0-9+\/]+={0,2}$'
   $password_error = "The password hash '${password}' is not valid."
   validate_re($password, $password_regex, $password_error)
 
